b58e170db414a08bd15a5d402f74978cb39595ab
Vaibhav <vaibhavrajpianist@gmail.com>
2022-01-17 13:41:57 +0530

    initial

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const baseURL = "https://innshare.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
-const maxAllowedSize = 100 * 1024 * 1024; //100mb
+const maxAllowedSize = 490 * 1024 * 1024; //100mb
 
 
 browseBtn.addEventListener("click", () => {
